889212c9cfc7ca92f8df511e9aa7982bfc4c764c
RajVignesh2512 <132724892+RajVignesh2512@users.noreply.github.com>
2023-05-08 21:47:01 +0530

    Add files via upload

---
+++ day1/Twowheeler.java
@@ -0,0 +1,40 @@
+package week2.day1;
+
+public class Twowheeler {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int noOfWheel=2;
+     System.out.println(noOfWheel);
+     short noOfMirrors=2;
+     System.out.println(noOfMirrors);
+     long chassisNumber=2239876590083683l;
+     System.out.println(chassisNumber);
+     boolean isPunctured=true;
+     System.out.println(isPunctured);
+     String bikeName="Honda Dream Yuga";
+     System.out.println(bikeName);
+     double runningKM=65000;
+     System.out.println(runningKM);
+     float airPressure=38.9f;
+     System.out.println(airPressure);
+     float fuelCapacity=12.2f;
+     System.out.println(fuelCapacity);
+     byte noOfIndicator=4;
+     System.out.println(noOfIndicator);
+     char bikeLogo='H';
+     System.out.println(bikeLogo);
+     short tripMeterKM=1200;
+     System.out.println(tripMeterKM);
+     byte maximumSpeed=65;
+     System.out.println(maximumSpeed);
+     long engineNumber=78647568936567893l;
+     System.out.println(engineNumber);
+     
+     
+    		 
+    		 
+    		 
+	}
+
+}
