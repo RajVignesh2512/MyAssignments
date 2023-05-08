cd1d34c34be5d9234f10dac045a200d2bcb545b3
RajVignesh2512 <132724892+RajVignesh2512@users.noreply.github.com>
2023-05-08 22:35:58 +0530

    Add files via upload

---
+++ day2/Mobile.java
@@ -0,0 +1,40 @@
+package week2.day1;
+
+public class Mobile {
+	String mobileBrand="LG";
+	char mobileLogo='L';
+	short noOfMobilePiece=26;
+	int modelNumber=67854;
+	long mobileImeiNumber=4756889528907568l;
+	float mobilePrice=70.000f;
+	boolean isDamaged=false;
+	short processorsSnapdragon=855;
+	String modelName="LG G8X Thinq";
+	short batteryCapacity=4000;
+	float androidVersion=12.0f;
+	short adrenoGPU=650;
+	float bluetoothVersion=5.0f;
+	String colour="Aurora BLACK";
+	float mobileSize=6.4f;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Mobile lg=new Mobile();
+    System.out.println(lg.mobileBrand);
+    System.out.println(lg.mobileLogo);
+    System.out.println(lg.noOfMobilePiece);
+    System.out.println(lg.modelNumber);
+    System.out.println(lg.mobileImeiNumber);
+    System.out.println(lg.mobilePrice);
+    System.out.println(lg.isDamaged);
+    System.out.println(lg.processorsSnapdragon);
+    System.out.println(lg.modelName);
+    System.out.println(lg.batteryCapacity);
+    System.out.println(lg.androidVersion);
+    System.out.println(lg.adrenoGPU);
+    System.out.println(lg.bluetoothVersion);
+    System.out.println(lg.colour);
+    System.out.println(lg.mobileSize);
+	}
+
+}
---
+++ day2/Twowheeler.java
@@ -0,0 +1,36 @@
+package week2.day1;
+
+public class Twowheeler {
+	int noOfWheel=2;
+	short noOfMirrors=2;
+	long chassisNumber=2239876590083683l;
+	boolean isPunctured=true;
+	String bikeName="Honda Dream Yuga";
+	double runningKM=65000;
+	float airPressure=38.9f;
+	float fuelCapacity=12.2f;
+	byte noOfIndicator=4;
+	char bikeLogo='H';
+	short tripMeterKM=1200;
+	byte maximumSpeed=65;
+	long engineNumber=78647568936567893l;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     Twowheeler honda=new Twowheeler();
+     System.out.println(honda.noOfWheel);
+     System.out.println(honda.noOfMirrors);
+     System.out.println(honda.chassisNumber);
+     System.out.println(honda.isPunctured);
+     System.out.println(honda.bikeName);
+     System.out.println(honda.runningKM);
+     System.out.println(honda.airPressure);
+     System.out.println(honda.fuelCapacity);
+     System.out.println(honda.noOfIndicator);
+     System.out.println(honda.bikeLogo);
+     System.out.println(honda.tripMeterKM);
+     System.out.println(honda.maximumSpeed);
+     System.out.println(honda.engineNumber);   		 
+	}
+
+}
